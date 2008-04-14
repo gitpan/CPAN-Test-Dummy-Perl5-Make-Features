3468a631643f137c5e987bf91f513f3c6773eaf3
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2008-04-14 05:47:39 +0000

    Import of ANDK/CPAN-Test-Dummy-Perl5-Make-Features-1.04 from CPAN.
    gitpan-cpan-distribution: CPAN-Test-Dummy-Perl5-Make-Features
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         ANDK/cpantestdummies/CPAN-Test-Dummy-Perl5-Make-Features-1.04.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- lib/CPAN/Test/Dummy/Perl5/Make/Features.pm
+++ lib/CPAN/Test/Dummy/Perl5/Make/Features.pm
@@ -2,7 +2,7 @@ use strict;
 
 package CPAN::Test::Dummy::Perl5::Make::Features;
 
-$CPAN::Test::Dummy::Perl5::Make::Features::VERSION = '1.03';
+$CPAN::Test::Dummy::Perl5::Make::Features::VERSION = '1.04';
 
 1;
 
